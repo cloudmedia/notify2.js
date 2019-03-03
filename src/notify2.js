483d350ea1d2b4e88d5e6429bb9783c8606c5026
Jay Simons <jay@cloudulus.net>
2019-03-03 01:01:39 -0600

    update

--- src/notify2.js
+++ src/notify2.js
@@ -10,22 +10,13 @@ var n2DefaultClass = "error";
 var n2DefaultDelay = 3000; // 3000 milliseconds = 3 seconds
 var n2DefaultAutoHide = true; // Auto hide notifications be default (true or false)
 var n2DefaultIcon = "fa-times-circle"; // Default FontAwesome icon
-if (useSounds)
-{
-    var n2DefaultSound = sndError; // Default sound from sounds.js
+if (useSounds) {
+    var n2DefaultSound = 'erroneous';
 }
 var n2DefaultMessage = "An unknown error occurred!"; // Default message (if none is supplied)
 
-$(document).click(function(e){
-    $(".notify2").each(function(){
-        hideNotify2($(this));
-    });
-});
-
-class Notify2
-{
-    constructor(msg, cls, autoHide, delay)
-    {
+class Notify2 {
+    constructor(msg, cls, autoHide, delay) {
         if (msg == undefined) msg = n2DefaultMessage;
         this.message = msg;
         if (cls == undefined) cls = n2DefaultClass;
@@ -43,137 +34,127 @@ class Notify2
         this.debug = false;
     }
 
-    setAutoHide(bool)
-    {
+    setAutoHide(bool) {
         this.autoHide = bool;
         return true;
     }
 
-    setDelay(delay)
-    {
+    setDelay(delay) {
         this.delay = delay;
         return true;
     }
 
-    setBindEvent(e)
-    {
+    setBindEvent(e) {
         this.bindEvent = e;
         return true;
     }
 
-    setDebug(bool)
-    {
+    setDebug(bool) {
         this.debug = bool;
         return true;
     }
 
-    doConfirm(cbYes, cbNo)
-    {
-        this.message='<div class="confirm"><div>'+this.message+'</div>'+
-            '<div><button id="notify2-confirm-yes">Yes</div>'+
+    doConfirm(cbYes, cbNo) {
+        this.message = '<div class="confirm"><div>' + this.message + '</div>' +
+            '<div><button id="notify2-confirm-yes">Yes</div>' +
             '<div><button id="notify2-confirm-no">No</button></div></div>';
         this.autoHide = false;
         this.cbYes = cbYes;
         this.cbNo = cbNo;
         var me = this;
-        if (typeof this.cbYes === 'function')
-        {
-            $(document).delegate('#notify2-confirm-yes', this.bindEvent, function(){
+        if (typeof this.cbYes === 'function') {
+            $(document).delegate('#notify2-confirm-yes', this.bindEvent, function () {
                 me.cbYes(me);
             });
         }
-        if (typeof this.cbNo === 'function')
-        {
-            $(document).delegate('#notify2-confirm-no', this.bindEvent, function(){
+        if (typeof this.cbNo === 'function') {
+            $(document).delegate('#notify2-confirm-no', this.bindEvent, function () {
                 me.cbNo(me);
             });
         }
         return true;
     }
 
-    notify()
-    {
+    notify() {
         var notify;
-        switch (this.class)
-        {
+        switch (this.class) {
             case "success":
                 this.icon = "fa-check-square";
-                if (useSounds) this.sound = sndSuccess;
-            break;
+                if (useSounds) this.sound = 'calculate';
+                break;
             case "warn":
                 this.icon = "fa-exclamation-triangle";
-                if (useSounds) this.sound = sndDing;
-            break;
+                if (useSounds) this.sound = 'dialex';
+                break;
             case "info":
                 this.icon = "fa-question-circle";
-                if (useSounds) this.sound = sndAlert1;
-            break;
+                if (useSounds) this.sound = 'chikaka';
+                break;
             case "mail":
                 this.icon = "fa-envelope";
-                if (useSounds) this.sound = sndMessage;
-            break;
+                if (useSounds) this.sound = 'whoop';
+                break;
         }
         if (this.debug) console.log(this.message);
         $('body').append(
-            '<div id="'+this.id+'" class="notify2 '+this.class+'" data-timer="" style="display: none;">'+
-            '<div><div><i class="fas '+this.icon+'"></i></div>'+
-            '<div>'+this.message+'</div></div>'+
+            '<div id="' + this.id + '" class="notify2 ' + this.class + '" data-timer="" style="display: none;">' +
+            '<div><div><i class="fas ' + this.icon + '"></i></div>' +
+            '<div>' + this.message + '</div></div>' +
             '<div><i class="fas fa-times" id="notify2-close-btn"></i></div></div>'
         );
-        notify = $("#"+this.id); 
-        notify.hide().click(function(){
+        notify = $("#" + this.id);
+        notify.hide().click(function () {
             hideNotify2($(this));
         });
 
-        if (this.autoHide)
-        {
-            if (this.debug) console.log("Notify2 Autohide: "+this.autoHide+", Delay: "+this.delay);
+        if (this.autoHide) {
+            if (this.debug) console.log("Notify2 Autohide: " + this.autoHide + ", Delay: " + this.delay);
             var hideID = this.id;
             var hideDelay = this.delay;
             var debug = this.debug;
-            setTimeout(function(){
-                var hideme = "#"+hideID;
-                if (debug) console.log("Hiding notification: "+hideme);
+            setTimeout(function () {
+                var hideme = "#" + hideID;
+                if (debug) console.log("Hiding notification: " + hideme);
                 $(hideme).click();
             }, hideDelay);
         }
 
         showNotify2(notify);
-        if (useSounds) playSound(this.sound);
+        if (useSounds) sounds.play(this.sound);
         return true;
     }
 }
-function showNotify2(notify)
-{
+
+function showNotify2(notify) {
     var height = notify.height();
     notify.css("margin-bottom", -height).show();
-    if(notify.css("margin-bottom") == -height+"px" && !notify.is(':animated'))
-    {
-        notify.animate({"margin-bottom": '+='+height});
-    }
-    else
-    {
-        if(!notify.is(':animated'))
-        {
-            notify.animate({"margin-bottom": '-='+height});
+    if (notify.css("margin-bottom") == -height + "px" && !notify.is(':animated')) {
+        notify.animate({
+            "margin-bottom": '+=' + height
+        });
+    } else {
+        if (!notify.is(':animated')) {
+            notify.animate({
+                "margin-bottom": '-=' + height
+            });
         }
     }
 }
-function hideNotify2(notify)
-{
+
+function hideNotify2(notify) {
     var timer = notify.attr("data-timer");
     if (timer) clearTimeout(timer);
-    playSound(sndType);
+    if (useSounds) sounds.play('blip');
     var height = notify.height() + 15;
-    if(notify.css("margin-bottom") == -height+"px" && !notify.is(':animated'))
-    {
-        notify.animate({"margin-bottom": '+='+height});
-    }
-    else
-    {
-        if(!notify.is(':animated'))
-        {
-            notify.animate({"margin-bottom": '-='+height}, function(){
+    if (notify.css("margin-bottom") == -height + "px" && !notify.is(':animated')) {
+        notify.animate({
+            "margin-bottom": '+=' + height
+        });
+    } else {
+        if (!notify.is(':animated')) {
+            notify.animate({
+                "margin-bottom": '-=' + height
+            }, function () {
                 $(this).remove();
             });
         }
@@ -181,8 +162,7 @@ function hideNotify2(notify)
 }
 
 // Shortcut function to easily instantiate on one line
-function notify2(msg, cls, ah)
-{
+function notify2(msg, cls, ah) {
     if (msg == undefined) msg = "";
     if (cls == undefined) cls = "error";
     if (ah == undefined) ah = true;
@@ -190,3 +170,41 @@ function notify2(msg, cls, ah)
     notify.setAutoHide(ah);
     notify.notify();
 }
+
+(function ($) {
+    $.fn.notifyField = function (opts, callback) {
+        var settings = $.extend({
+            message: "An error occurred!",
+            class: 'error',
+            autoHide: false,
+            delay: 3000,
+            doConfirm: false,
+            callBackYes: null,
+            callBackNo: null,
+            clearField: false,
+            scrollToField: true,
+            topOffset: 0,
+            scrollTime: 1000
+        }, opts);
+
+        var n = new Notify2(settings.message, settings.class, settings.autoHide, settings.delay);
+        if (settings.doConfirm) n.doConfirm(settings.callBackYes, settings.callBackNo);
+
+        if (settings.scrollToField) {
+            var offSet = 0;
+            if (typeof this.offset === 'function') offSet = this.offset().top;
+            $('html, body').animate({
+                scrollTop: (offSet - settings.topOffset)
+            }, settings.scrollTime);
+        }
+
+        if (settings.clearField) this.val("");
+        this.focus();
+        n.notify();
+        if (typeof callback === 'function') {
+            return callback(this);
+        } else {
+            return this;
+        }
+    }
+})(jQuery);
